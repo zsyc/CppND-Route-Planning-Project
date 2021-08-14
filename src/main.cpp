89fd972064c04ace44519d205fe3b6e5d6346d6a
zsyc <33007708+zsyc@users.noreply.github.com>
2021-08-15 00:01:06 +0200

    revise according to the advices from reviewer

--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,12 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
     float start_x, start_y, end_x, end_y;
-    std::cout << "Please input the coordinate: start_x start_y end_x end_y:\n";
-    std::cin >> start_x >> start_y >> end_x >>end_y;
+    do{
+        std::cout << "Please input the coordinate: start_x start_y end_x end_y in the range 0~100:\n";
+        std::cin >> start_x >> start_y >> end_x >>end_y;
+    }
+    while (start_x >= 0 and start_y >= 0 and end_x >=0 and end_y>=0 \
+           and start_x <=100 and start_y <=100 and end_x <=100 and end_y<=100);
     
     // Build Model.
     RouteModel model{osm_data};

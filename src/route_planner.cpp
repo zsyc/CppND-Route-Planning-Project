9416e5abc0609f82ab33346c51046dbcb5e3b1f6
zsyc <33007708+zsyc@users.noreply.github.com>
2021-08-14 23:55:35 +0200

    revise the code according to the advices from reviewer

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -35,12 +35,12 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
     current_node->FindNeighbors();
-    for (RouteModel::Node *nd : current_node->neighbors) {
+    for (RouteModel::Node &nd : current_node->neighbors) {
         nd->parent = current_node;
         nd->h_value = CalculateHValue(nd);
         nd->g_value = current_node->g_value + current_node->distance(*nd);
-        open_list.push_back(nd);
         nd->visited = true;
+        open_list.emplace_back(nd);
     }
 }
 
@@ -72,22 +72,19 @@ RouteModel::Node *RoutePlanner::NextNode() {
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
-    std::vector<RouteModel::Node> path_found, tmp;;
+    std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
     while (current_node != start_node){
-        tmp.push_back(*current_node);
+        path_found.emplace_back(*current_node);
         distance += current_node->distance(*current_node->parent);
         current_node = current_node->parent;
     }
-    tmp.push_back(*start_node);
-    for (int i = tmp.size()-1; i >= 0; --i)
-        path_found.push_back(tmp[i]);
+    path_found.emplace_back(*start_node);
+    std::reverse(path_found.begin(), path_found.end());
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
-
 }
-
 
 // TODO 7: Write the A* Search algorithm here.
 // Tips:
